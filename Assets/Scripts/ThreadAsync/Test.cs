c058264e5dda7d2eea2d2e77d51e3d5bc146f53d
liujunjie612 <1225325960@qq.com>
2017-04-25 11:39:49 +0800

    线程的使用

---
+++ Assets/Scripts/ThreadAsync/Test.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ThreadAsync
+{
+    public class Test : MonoBehaviour
+    {
+        public GameObject perfab;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.C))
+            {
+                clone();
+            }
+
+            if(Input.GetKeyDown(KeyCode.B))
+            {
+                //注意支线程不能调用Unity API函数
+                Loom.RunAsync(clone);
+            }
+        }
+
+       private void clone()
+        {
+           for(int i = 0;i<10000;i++)
+           {
+               Debug.Log("yyyyyyyyyyyyy");
+           }
+        }
+
+    }
+}
