f2a80ab37c0edbe0246f36a2806cbcec8f8e4738
liujunjie612 <1225325960@qq.com>
2017-05-08 13:47:24 +0800

    显示圆形图片（不用shader）

---
+++ Assets/Scripts/RawImageCircle/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace RawImageCircleFloder
+{
+
+    public class Test : MonoBehaviour
+    {
+
+        public RawImageCircle img;
+
+        void Start()
+        {
+            EventTriggerListener.Get(img.gameObject).onClick = this.__onClick;
+        }
+
+        private void __onClick()
+        {
+            Debug.Log("Click");
+        }
+    }
+}
